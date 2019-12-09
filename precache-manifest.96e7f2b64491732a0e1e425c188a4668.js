13b40672a2c047915a2dc57a15ea2990f0e08274
jstapleton463 <42473264+jstapleton463@users.noreply.github.com>
2019-12-08 22:09:39 -0500

    Initial upload

---
+++ precache-manifest.96e7f2b64491732a0e1e425c188a4668.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c7ea2fc633709841a0",
+    "url": "/css/chunk-vendors.f880acea.css"
+  },
+  {
+    "revision": "eb96df2f8a466e6d06ad2b577e2813ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76068aa24b7613b6426c",
+    "url": "/js/about.780bd949.js"
+  },
+  {
+    "revision": "491b48cfca183e67626e",
+    "url": "/js/app.c470d05d.js"
+  },
+  {
+    "revision": "c4c7ea2fc633709841a0",
+    "url": "/js/chunk-vendors.76a5a955.js"
+  },
+  {
+    "revision": "b70db8241eddead4fcc9da971ac31aea",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
